d8b3b13e4ce6d1068281b79b4b4e79ab2b2f625f
alWasayf <49132323+alWasayf@users.noreply.github.com>
2022-08-01 16:47:13 +0300

    twitter-project

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Homepage } from './pages/home';
+import { Loginpage } from './pages/login';
+import { Profilepage } from './pages/profile';
+import { Explorepage } from './pages/explore';
 import './index.css';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    {/* your router goes here */}
-  </React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+		<Route path='/' element={<Homepage />} />
+			<Route path='/profile' element={<Profilepage />} />
+			<Route path='/login' element={<Loginpage />} />
+      <Route path='/explore' element={<Explorepage />} />
+		</Routes>
+	</BrowserRouter>
 );
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,4 @@
+export function Homepage() {
+	return(
+        <div>Home Page</div>
+    )}
