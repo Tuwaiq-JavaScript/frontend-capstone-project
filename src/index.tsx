78a7fb75fafb55a4ce7959bb2b9f9de1b19918e2
Essa <essa1417323@gmail.com>
2022-08-02 02:23:36 +0300

    commit did 90% from profile page

--- src/index.tsx
+++ src/index.tsx
@@ -4,25 +4,25 @@ import {
 	BrowserRouter,
 	Routes,
 	Route,
-  } from "react-router-dom";
+} from "react-router-dom";
 import { LoginPage } from './pages/login';
-import { ProfilePage } from './pages/profile';
+import { Profiles } from './pages/profile';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<>
 
-	<BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage/>} />
-	  <Route path="/home" element={<LoginPage/>}/>
-      <Route path="/sign-up" element={<LoginPage/>} />
-      <Route path="/login" element={<LoginPage/>}/>
-	  <Route path="/profile" element={<ProfilePage/>}/>
-	  <Route path="/company" element={<LoginPage/>}/>
-	  
-    </Routes>
-  </BrowserRouter>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				{/* <Route path="/home" element={<LoginPage />} />
+				<Route path="/sign-up" element={<LoginPage />} />
+				<Route path="/login" element={<LoginPage />} /> */}
+				<Route path="/profile" element={<Profiles />} />
+				{/* <Route path="/company" element={<LoginPage />} /> */}
+
+			</Routes>
+		</BrowserRouter>
 	</>
 );
