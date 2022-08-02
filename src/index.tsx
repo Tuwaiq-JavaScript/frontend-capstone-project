f3b94ac6c5be0ddde6aaeb30d0cb318acaa6a82f
A1I21 <87023979+A1I21@users.noreply.github.com>
2022-08-02 10:24:07 +0300

    Navbar is done

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-	BrowserRouter,
-	Routes,
-	Route,
-  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Nav } from './components/Nav';
+import { HomePage } from './pages/home';
 import { LoginPage } from './pages/login';
 import './styles/index.css';
+import './styles/navbar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<>
-
-	<BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage/>} />
-	  <Route path="/home" element={<LoginPage/>}/>
-      <Route path="/sign-up" element={<LoginPage/>} />
-      <Route path="/login" element={<LoginPage/>}/>
-	  <Route path="/profile" element={<LoginPage/>}/>
-	  <Route path="/company" element={<LoginPage/>}/>
-	  
-    </Routes>
-  </BrowserRouter>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<LoginPage />} />
+				<Route path='/home' element={<LoginPage />} />
+				<Route path='/sign-up' element={<LoginPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/profile' element={<LoginPage />} />
+				<Route path='/company' element={<LoginPage />} />
+			</Routes>
+		</BrowserRouter>
 	</>
 );
