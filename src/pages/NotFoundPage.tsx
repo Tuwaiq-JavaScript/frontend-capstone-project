a8deee45314b71280073f63d76e0ad6c51817ebe
Essa <essa1417323@gmail.com>
2022-08-03 20:28:25 +0300

    header-cmpanyProfile

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import "../styles/index.css"
+export function NotFoundPage() {
+
+  return (
+    <>
+      <div className="box20">
+        <p className="bigtext">404</p>
+        <p className="notfound">Page Not Found</p>
+      </div>
+    </>
+  )
+
+}
