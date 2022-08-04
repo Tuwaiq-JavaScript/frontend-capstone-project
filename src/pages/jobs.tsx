b5375c34a17e45da182c6798c1dfc86959bb5345
meshal1172 <mr.meeshal@hotmail.com>
2022-08-04 15:30:29 +0300

    upduate jobs page

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -1,12 +1,13 @@
 import {Recommendeds} from '../components/jobs/Jobs'
 import { Sidebar } from '../components/jobs/Sidebar';
+import { Nav } from '../components/Nav';
 
 
 export function JobsPage() {
 	return (
 		<>
 		<div>
-
+                < Nav />
 				<Sidebar />
 				<Recommendeds />
 		</div>
