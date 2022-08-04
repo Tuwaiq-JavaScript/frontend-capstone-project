90866a84d81419607a65ef6aa1625274c7b84644
Ghaida moh <gigi@test.com>
2022-08-04 19:33:53 +0300

    profile-login-update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,64 @@
-export function Profile()
-{
-    return(
-        <>
-        <h1>profile page</h1>
-        </>
-    )
+import  Header from "./Header"
+import {Sidebar}  from "./Sidebar";
+import "./styles/index.css";
+
+// #212121
+// #181818
+
+const localUser = JSON.parse(localStorage.getItem("user") || "null");
+const username = localUser ? localUser.email.split("@")[0] : null;
+
+interface TapProps {
+  title: string;
 }
+
+const Tap = ({ title }: TapProps) => {
+  return (
+    <div className="profile__tap">
+      <p>{title}</p>
+    </div>
+  );
+};
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <Header />
+      <main>
+        <Sidebar />
+        <div className="profile__content">
+          <div className="profile__contentTop">
+            <div className="profile__info">
+              <div className="profile__avatar">{username[0]}</div>
+              <div className="profile__username">{username}</div>
+              <div className="profile__btns">
+                <button>Custom channel</button>
+                <button>Manage videos</button>
+              </div>
+            </div>
+            <div className="profile__taps">
+              <Tap title="home" />
+              <Tap title="videos" />
+              <Tap title="playlists" />
+              <Tap title="channels" />
+              <Tap title="about" />
+            </div>
+          </div>
+          <div className="profile__contentBot">
+            <div className="profile__empty">
+              <img src="/images/empty-profile.svg" alt="empty-svg" />
+              <h2>Upload a video to get started</h2>
+              <p>
+                Start sharing your story and connecting with viewers. Videos you
+                upload will show up here.
+              </p>
+              <button>upload video</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Profile;
