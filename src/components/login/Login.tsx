13026103352754c048be8d19cd028d9c504bf1a6
meshal1172 <mr.meeshal@hotmail.com>
2022-08-06 18:18:06 +0300

    update-login-signup-jobs

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+import '../../styles/login.css'
+
+export function Login() {
+
+	return (
+        <>
+        <div className="container-login-page">
+        <h2>Linked<span>
+            <i className="fab fa-linkedin"></i>
+        </span></h2>
+        <div className="text-login-page">
+            <h1>Sign in</h1>
+            <p>Stay updated on your professional world</p>
+        </div>
+        <div className="your-input-login-page">
+            <div className="input-login-page">
+                <input type="text" name="email" 
+                    id="email" required />
+                <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-login-page">
+                <input type="password" name="password" 
+                    id="password" required />
+                <label htmlFor="password">
+                    Password
+                </label>
+            </div>
+        </div>
+        <a href="https://www.linkedin.com/checkpoint/rp/request-password-reset?trk=guest_homepage-basic_nav-header-signin" className="forgot-password-link-login-page">
+            Forgot Password?
+        </a>
+        <button className="button-login-page">Sign in</button>
+        <p className="join-link-login-page">
+            New to linkedin?
+            <Link to={'/sign-up'}>
+            <a href="#" className="join-now-login-page">
+                Join now
+            </a>
+            </Link>
+        </p>
+    </div>
+        </>
+
+    )
+    }
