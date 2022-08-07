132b608e801b85ee5f70a28c9c2ca5185e871887
فواز عسيري <Fowazassiri@gmail.com>
2022-08-07 11:10:13 +0300

    update_links

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
 	return (
 		<div className="login">
@@ -8,11 +10,15 @@ export default function Login() {
 			<div className="login-container">
 				<form className="login-form">
 					<input type="text" placeholder="Email or phone number" />
-					<input type="text" placeholder="Password" />
+					<input type="password" placeholder="Password" />
+					<Link to={'/'}>
 					<button className="login-btn-login">Log In</button> <br />
+					</Link>
 					<a href="#">Forgot password</a>
 					<div className="login-underline">____________________________________________________</div>
+					<Link to={'/signup'}>
 					<button className="login-btn-create-acc">Create new account</button>
+					</Link>
 				</form>
 				<h4 className="login-h4">
 					<a className="login-a" href="#">
